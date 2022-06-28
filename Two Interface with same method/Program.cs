e02c5e51009bb08cc45285bdc53c2ce0f0e42633
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 02:03:07 +0530

    Add project files.

---
+++ Two Interface with same method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Two_Interface_with_same_method
+{
+    interface F1
+    {
+        public abstract void M1();
+    }
+    interface F2
+    {
+        void M1();
+    }
+    class Program:F1,F2
+    {
+        public void M1()
+        {
+            Console.WriteLine("Same Mehtod :");
+        }
+      
+    }
+    class main:Program
+    {
+        static void Main(string[] args)
+        {
+            F1 obj = new Program();
+            F2 obj1 = new Program();
+            obj.M1();
+            obj1.M1();
+            Program obj2 = new Program();
+            main obj3 = new main();
+            obj2.M1();
+            obj3.M1();
+        }
+    }
+}
